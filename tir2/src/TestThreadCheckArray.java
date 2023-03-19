3c65a3502fe77a024ceaca45c4cd20f448ccfeda
PC <PC@10.10.6.154>
2023-03-19 12:33:36 +0200

    hi2

--- tir2/src/TestThreadCheckArray.java
+++ tir2/src/TestThreadCheckArray.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 //hello
-public class TestThreadCheckArray {
+public class TestThreadCheckArray {//
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)) {
 			Thread thread1, thread2;
