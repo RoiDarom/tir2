219af8e32da78d9ad3b910d461296400be7c1656
roida <roida@Roi-PC>
2023-03-19 12:43:36 +0200

    cahnafsa

--- tir2/src/TestThreadCheckArray.java
+++ tir2/src/TestThreadCheckArray.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//hello
+//hello//
 public class TestThreadCheckArray {//
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)) {
